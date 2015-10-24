bed2088c73bee7b3b8d15f02e248cee590ae22e2
chweeks <chrisharryweeks@gmail.com>
2015-10-24 15:40:36 +0100

    added first test and protractor installed

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -19,7 +19,8 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-mocks/angular-mocks.js',
-      'js/**/*.js',
+      'js/app.js',
+      'toDolistController.js',
       'test/**/*.spec.js'
     ],
 
