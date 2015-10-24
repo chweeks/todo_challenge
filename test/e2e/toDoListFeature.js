c2e5616c75a47122eadbf4dc1ba1dc41203514b1
chweeks <chrisharryweeks@gmail.com>
2015-10-24 20:28:51 +0100

    completed tasks now hidden

--- test/e2e/toDoListFeature.js
+++ test/e2e/toDoListFeature.js
@@ -1,7 +1,9 @@
 describe('To Do list maker', function() {
 
-  var inputBox = element(by.model('toDoCtrl.taskInput'));
+  var inputBox = element(by.model('string'));
   var addTaskButton = element(by.id('addTask'));
+  var completeTaskButton = element(by.id('completeTask'));
+  var tasks = element.all(by.repeater('task in toDoCtrl.tasks'));
 
   beforeEach(function() {
     browser.get('http://localhost:8080');
@@ -15,15 +17,19 @@ describe('To Do list maker', function() {
 
     inputBox.sendKeys('Do Homework');
     addTaskButton.click();
-
-    var tasks = element.all(by.repeater('task in toDoCtrl.tasks'));
     expect(tasks.get(0).getText()).toEqual('Do Homework Completed');
   });
 
   it('has a completed task button when tasks are added', function(){
     inputBox.sendKeys('Do Homework');
     addTaskButton.click();
-    var completeTaskButton = element(by.id('completeTask'));
     expect((completeTaskButton).isDisplayed()).toBeTruthy();
+  });
+
+  it('hides task when completed button is pressed', function(){
+    inputBox.sendKeys('Do Homework');
+    addTaskButton.click();
+    completeTaskButton.click();
+    expect((tasks).isDisplayed()).toBeFalse;
   });
 });
--- toDoListController.js
+++ toDoListController.js
@@ -3,9 +3,14 @@ toDoList.controller("ToDoListController", [function(){
   var self = this;
 
   self.tasks = [];
+  self.completedTasks = [];
 
-  self.addTask = function(){
-    self.tasks.push(self.taskInput);
+  self.addTask = function(string){
+    self.tasks.push({ task: string,
+                      completed: false });
   };
 
+  self.completedTask = function(task){
+    task.completed = true;
+  };
 }]);
