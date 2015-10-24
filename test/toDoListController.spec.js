acc845767a3b39368a8dc96b2101623e2b52ed92
chweeks <chrisharryweeks@gmail.com>
2015-10-24 16:01:15 +0100

    added task input and display

--- test/toDoListController.spec.js
+++ test/toDoListController.spec.js
@@ -7,7 +7,20 @@ describe('ToDoListController', function() {
     ctrl = $controller('ToDoListController');
   }));
 
-  it('initialises with an empty search result and term', function() {
-    expect(ctrl.toDoInput).toBeUndefined();
+  it('initialises with an empty task input', function() {
+    expect(ctrl.taskInput).toBeUndefined();
+  });
+
+  it('initialises with an empty tasks array', function() {
+    expect(ctrl.tasks).toEqual([]);
+  });
+
+  describe('when inputting new task', function() {
+
+    it('displays task', function() {
+      ctrl.taskInput = 'do homework';
+      ctrl.addTask();
+      expect(ctrl.tasks).toEqual(['do homework']);
+    });
   });
 });
--- toDoListController.js
+++ toDoListController.js
@@ -1,3 +1,11 @@
 toDoList.controller("ToDoListController", [function(){
 
+  var self = this;
+
+  self.tasks = [];
+
+  self.addTask = function(){
+    self.tasks.push(self.taskInput);
+  };
+
 }]);
